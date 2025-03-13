5f35873a587b0e1bdb0e61814b62d5cd0e4b8a92
Brett Anda <brett@brettanda.ca>
2025-03-12 19:55:04 -0600

    added proxy disconnect message when lobby server is starting

--- src/main/java/de/tubyoub/velocitypteropower/VelocityPteroPower.java
+++ src/main/java/de/tubyoub/velocitypteropower/VelocityPteroPower.java
@@ -230,6 +230,12 @@ public class VelocityPteroPower {
             return;
         }
         if (startingServers.contains(serverName)){
+            if (event.getPreviousServer() == null) {
+                player.disconnect(
+                    Component.text(
+                        "Starting server...\n\nThe " + serverName + " server is still booting up\n\nPlease try again in a minute or two.",
+                        NamedTextColor.WHITE));
+             }
             player.sendMessage(
                 this.getPluginPrefix()
                 .append(Component.text( messagesManager.getMessage("server-starting")
@@ -245,6 +251,12 @@ public class VelocityPteroPower {
                 .append(Component.text(messagesManager.getMessage("starting-server")
                         .replace("%server%", serverName), NamedTextColor.WHITE)));
         event.setResult(ServerPreConnectEvent.ServerResult.denied());
+        if (event.getPreviousServer() == null) {
+                player.disconnect(
+                    Component.text(
+                        "Starting server…\n\nServer " + serverName + " is currently in hibernation mode to conserve server resources.\nIt is now being started, so please wait a moment and then reconnect.",
+                        NamedTextColor.WHITE));
+         }
 
         proxyServer.getScheduler().buildTask(this, () -> {
             if (apiClient.isServerOnline(serverName) && this.canMakeRequest()) {
